38c91f3f022f8ac28b7ca3174e4cd466ff49b0af
Hung-Su <hungsu.nguyen@gmail.com>
2016-05-24 08:38:31 -0400

    Use recursion to get all steam OP games

--- src/scrape.js
+++ src/scrape.js
@@ -1,37 +1,60 @@
 var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: false })
 
-nightmare
-	.goto('http://store.steampowered.com/search/#sort_by=Reviews_DESC&page=1')
-	// .click('form[action*="/search"] [type=submit]')
-	.wait(375) // Wait for Steam's javascript to fetch and render results
-	.evaluate(function () {
-		// Now that we're on the page with the right results, build an array
-		var gameElements = [].slice.call(document.querySelectorAll('#search_result_container .search_result_row'))
-		var games = gameElements.map(function(row){
-			var rawPrice = row.querySelector('.search_price_discount_combined .search_price').textContent;
-			var cleanPrice = rawPrice.replace(/\t/g, "").replace(/\n/g, "")
-			var hasDollar = cleanPrice.lastIndexOf('$')
-			if (hasDollar) {
-				cleanPrice = cleanPrice.substring(hasDollar)
+/*
+
+*/
+var steamGames = []
+
+function getGamesFromPage(pageNumber){
+	return new Nightmare({ show: true })
+		.goto('http://store.steampowered.com/search/#sort_by=Reviews_DESC&page='+pageNumber)
+		.wait(600) // Wait for Steam's javascript to fetch and render results
+		.evaluate(
+			function () {
+				// Now that we're on the page with the right results, build an array
+				var pageGameElements = [].slice.call(document.querySelectorAll('#search_result_container .search_result_row'))
+				var pageGames = pageGameElements.map(function(row){
+					var rawPrice = row.querySelector('.search_price_discount_combined .search_price').textContent;
+					var cleanPrice = rawPrice.replace(/\t/g, "").replace(/\n/g, "")
+					var hasDollar = cleanPrice.lastIndexOf('$')
+					if (hasDollar) {
+						cleanPrice = cleanPrice.substring(hasDollar)
+					}
+					var rawRating = row.querySelector('.search_review_summary').attributes["data-store-tooltip"].value;
+					return {
+						title: row.querySelector('.title').textContent,
+						reviewPositivePercent: rawRating.substring(rawRating.indexOf('>')+1,rawRating.indexOf('%')),
+						reviewTotal: rawRating.substring(rawRating.indexOf('the ')+4,rawRating.indexOf(' user')),
+						reviewConsensus: rawRating.substring(0,rawRating.indexOf('<')),
+						url: row.href,
+						price: cleanPrice
+					}
+				})
+				return pageGames
 			}
-			var rawRating = row.querySelector('.search_review_summary').attributes["data-store-tooltip"].value;
-			return {
-				title: row.querySelector('.title').textContent,
-				reviewPositivePercent: rawRating.substring(rawRating.indexOf('>')+1,rawRating.indexOf('%')),
-				reviewTotal: rawRating.substring(rawRating.indexOf('the ')+4,rawRating.indexOf(' user')),
-				reviewConsensus: rawRating.substring(0,rawRating.indexOf('<')),
-				url: row.href,
-				price: cleanPrice
+		)
+		.end()
+		.then(function (results) {
+			steamGames = steamGames.concat(results)
+			// console.log(steamGames)
+
+			// if the last game in the list is not OP, stop the loop
+			var allGamesAreOP = (steamGames[steamGames.length-1].reviewConsensus == 'Overwhelmingly Positive')
+			// allGamesAreOP = false;
+			if (allGamesAreOP) {
+				return getGamesFromPage(pageNumber+1)
+			} else {
+				return results
 			}
 		})
-		return games
-	})
-	.end()
-	.then(function (result) {
-		//Once array is built, write that to file
-		console.log(result)
-	})
-	.catch(function (error) {
-		console.error('Search failed:', error);
-	});
+		.catch(function (error) {
+			console.error('Search failed:', error);
+		});
+}
+
+getGamesFromPage(1).then(function(){
+	console.log(steamGames)
+	console.log(steamGames.length + " were found")
+})
+
+//Once array is built, write that to file
